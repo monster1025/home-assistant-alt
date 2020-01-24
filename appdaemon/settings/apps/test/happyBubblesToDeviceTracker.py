9221b3c9b2bde095935237fe4ba93d72da7232d5
Your Name <you@example.com>
2020-01-24 12:21:56 +0000

    updating configuration

--- appdaemon/settings/apps/test/happyBubblesToDeviceTracker.py
+++ appdaemon/settings/apps/test/happyBubblesToDeviceTracker.py
@@ -3,15 +3,13 @@ import appdaemon.plugins.mqtt.mqttapi as mqtt
 import globals
 import json
 import random
+import time
+import datetime
 
-"""
-Monitor events and output changes to the verbose_log. Nice for debugging purposes.
-Arguments:
- - events: List of events to monitor
-"""
-
-class HappyBubblesToDeviceTracker(mqtt.Mqtt):
+class HappyBubblesToDeviceTracker(mqtt.Mqtt, hass.Hass):
     base_topic = "happy-bubbles/presence/ha"
+    tag_timers = {}
+
     def initialize(self) -> None:
         self.log('Initialize HappyBubblesToDeviceTracker')
         self.mqtt_subscribe('{}/#'.format(self.base_topic), namespace='mqtt')
@@ -19,8 +17,8 @@ class HappyBubblesToDeviceTracker(mqtt.Mqtt):
     
     def event_listener(self, event_name, data, kwargs):
         topic = data.get('topic', '')
-        if self.base_topic	not in topic:
-        	return
+        if self.base_topic    not in topic:
+            return
         location = topic.replace('{}/'.format(self.base_topic), "")
 
         payload = json.loads(data['payload']) if data.get('payload') else {}
@@ -29,7 +27,41 @@ class HappyBubblesToDeviceTracker(mqtt.Mqtt):
         name = payload.get('name', None)
         distance = payload.get('distance', None)
 
-        hb_topic="happy-bubbles/location/{}".format(id)
-        self.mqtt_publish(hb_topic, location, qos = 0, retain = False, namespace='mqtt')
+        # devices = self.get_state()
+        # device_name = "device_tracker.tag_ble_{}".format(id)
+        # if device_name in devices:
+        #     self.log('tag found in devices')
+        
+        self.move_tag(id, isHome=True)
+        self.restart_timer(id)
         
         self.log('Beacon {} ({}) moved to {} (distance: {}).'.format(name, id, location, distance))
+    
+    def restart_timer(self, id):
+        if id in self.tag_timers:
+            self.cancel_timer(self.tag_timers[id])
+        self.tag_timers[id] = self.run_in(self.tag_away_timer_callback, 60, id=id)
+    
+    def tag_away_timer_callback(self, kwargs):
+        id = kwargs.get('id', None)
+        if id == None:
+            self.log('id is forgotten')
+            return
+
+        self.log('awaying tag:{}'.format(id))
+        self.move_tag(id, isHome=False)
+
+    def move_tag(self, id, isHome = False):
+        hb_topic = "owntracks/tag/ble_{}".format(id)
+        tst = int(round(time.time(),0))
+        payload = '{"_type":"location","acc":18,"alt":0,"batt":100,"lat":55.7899036,"lon":37.6595347,"tst":1579857481}'
+        jsobj = json.loads(payload)
+        jsobj['tst'] = tst
+        if isHome:
+            jsobj['lat'] = self.args['home_lat']
+            jsobj['lon'] = self.args['home_lon']
+        else:
+            jsobj['lat'] = 0
+            jsobj['lon'] = 0
+        payload = json.dumps(jsobj)
+        self.mqtt_publish(hb_topic, payload, qos = 0, retain = False, namespace='mqtt')        
