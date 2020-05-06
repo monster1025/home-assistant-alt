a37c56682225e3dbba548a16c5208de613cdf75b
Your Name <you@example.com>
2020-05-06 06:56:59 +0000

    water leak

--- homie/water-v2/src/main.ino
+++ homie/water-v2/src/main.ino
@@ -4,6 +4,7 @@
 
 
 HomieNode bathNode("bath", "node");
+bool initDone = false;
 
 bool resetHandler(String value) 
 {
@@ -20,10 +21,7 @@ void setupHandler() {
   bathNode.subscribe("cold", cold_waterInputHandler);
   Serial.begin(115200);
 
-  setup_valve();
-  setup_counters();
-//  setup_sonar();
-  initValve();
+  initDone = false;
 }
 
 void loopHandler() {
@@ -40,9 +38,47 @@ void setup() {
   Homie.registerNode(bathNode);
   Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
+  Homie.onEvent(onHomieEvent);
   Homie.setup();
 }
 
 void loop() {
   Homie.loop();
+  
+  if (!initDone && Homie.isReadyToOperate()) {
+    Serial.println("Running init event");
+    setup_valve();
+    setup_counters();
+    initValve();
+    initDone = true;    
+  }
+}
+
+void onHomieEvent(HomieEvent event) {
+  switch(event) {
+    case HOMIE_CONFIGURATION_MODE:
+      Serial.println("Configuration mode.");
+      break;
+    case HOMIE_NORMAL_MODE:
+      Serial.println("Normal mode.");
+      break;
+    case HOMIE_OTA_MODE:
+      Serial.println("OTA mode.");
+      break;
+    case HOMIE_ABOUT_TO_RESET:
+      Serial.println("Reset mode.");
+      break;
+    case HOMIE_WIFI_CONNECTED:
+      Serial.println("Wifi connected.");
+      break;
+    case HOMIE_WIFI_DISCONNECTED:
+      Serial.println("Wifi disconnected.");
+      break;
+    case HOMIE_MQTT_CONNECTED:
+      Serial.println("MQTT connected.");
+      break;
+    case HOMIE_MQTT_DISCONNECTED:
+      Serial.println("MQTT disconnected.");
+      break;
+  }
 }
