3c6d252c3dd14ce0eea1ae38aa750d1b109261e5
Your Name <you@example.com>
2020-09-02 08:28:49 +0300

    updating configuration

--- sls/settings/test.lua
+++ sls/settings/test.lua
@@ -1,1 +1,4 @@
-mqtt.pub('home/yandexStation/say', 'Здрасьте!')
+-- mqtt.pub('home/yandexStation/say', 'Здрасьте!')
+if Event.State.Value == "single" then
+   zigbee.set("0x00158D0001A4CFB4", "state", "toggle")
+end
